cf7da9ae5ba09e8db70a988497dcd99c8b0b9a6a
benjamin ojeda <benjamin@valuelist.cl>
2023-10-05 15:09:49 -0300

    Update backend dockerFl and create gunicorn deploy

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "API alpha version"
+    return "API alpha version!"
+
+@app.route("/users")
+def users():
+    users = [
+        {"id": 1, "name": "John Doe"},
+        {"id": 2, "name": "Jane Doe"},
+    ]
+    return jsonify(users)
 
 if __name__ == "__main__":
     app.run(debug=True)
