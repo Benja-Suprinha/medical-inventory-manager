73385ff7f82cfa2ec64f57d1c7a345c498a44862
benjamin ojeda <benjamin@valuelist.cl>
2023-10-13 14:58:28 -0300

    Add template for frontend dashboard

--- Frontend/my-app/src/app/app-routing.module.ts
+++ Frontend/my-app/src/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SesionComponent } from './layout/public/sesion/sesion.component';
 import { MainComponent } from './layout/private/main/main.component';
+import { AuthRoutesService } from './core/authRoutes/auth-routes.service';
 
 const routes: Routes = [{
-  path:'noauth', component: SesionComponent, loadChildren: () => import("./modules/login/login.module").then(m => m.LoginModule)
+  path:'noauth', 
+  component: SesionComponent, 
+  loadChildren: () => import("./modules/login/login.module").then(m => m.LoginModule)
 },
 {
-  path:'auth', component: MainComponent, loadChildren: () => import("./modules/principal/principal.module").then(m => m.PrincipalModule)
+  path:'auth', 
+  component: MainComponent, 
+  canActivate:[AuthRoutesService], 
+  loadChildren: () => import("./modules/principal/principal.module").then(m => m.PrincipalModule)
 },
 {
   path:'**', redirectTo:'noauth/login'
