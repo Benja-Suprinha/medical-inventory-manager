a2d37ef33c0c23d4449ab1e73362b8352978ae99
benja <benjamin.ojeda@mail.udp.cl>
2023-10-18 20:51:55 -0300

    Update a createUser form and return data form

--- Frontend/material-dashboard-angular2-master/src/app/modules/principal/pages/user-profile/user-profile.component.ts
+++ Frontend/material-dashboard-angular2-master/src/app/modules/principal/pages/user-profile/user-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-profile',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
+  public myForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private routeSv: Router
+  ) { }
 
   ngOnInit() {
+    this.myForm = this.createMyForm() 
+  }
+
+  createMyForm(): FormGroup{
+    return this.fb.group({
+      username:[''],
+      mail:[''],
+      password:[''],
+      name:[''],
+      type_user:[''],
+      phone:['']
+    })
+  }
+
+  submitFormulary(){
+    console.log(this.myForm.value)
   }
 
 }
