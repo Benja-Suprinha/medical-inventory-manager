a9f7f8ecc571eda21a5b4b65b1aca4d66c6509a7
benjamin <benjamin@valuelist.cl>
2023-10-23 14:21:28 -0300

    Fix updateUser password error

--- Frontend/material-dashboard-angular2-master/src/app/modules/principal/pages/table-list/table-list.component.ts
+++ Frontend/material-dashboard-angular2-master/src/app/modules/principal/pages/table-list/table-list.component.ts
@@ -17,7 +17,7 @@ export class TableListComponent implements OnInit {
   closeResult = '';
 
   constructor(
-    private adminService:AdminService,
+    private adminService: AdminService,
     public modal: NgbModal,
     private fb: FormBuilder,
   ) { }
@@ -26,59 +26,59 @@ export class TableListComponent implements OnInit {
     this.initTable()
   }
 
-  async initTable(){
+  async initTable() {
     const data: any = await this.adminService.Users().toPromise()
     this.waiting = false
     this.users = data
     console.log(this.users)
     this.dtOptions = {
       language: {
-        "search":"",
-        "searchPlaceholder":"Filtrar...",
+        "search": "",
+        "searchPlaceholder": "Filtrar...",
         "zeroRecords": "No se encuentran registros",
         "info": "Mostrando _START_ a _END_ de _TOTAL_ registros",
         "lengthMenu": "Mostrando _MENU_ registros",
-        "paginate":{
+        "paginate": {
           "first": "",
           "last": "",
           "previous": "Anterior",
           "next": "Siguiente"
         },
         "infoEmpty": "Mostrando 0 a 0 de 0 registros",
-        "infoFiltered":   "(filtrado de _MAX_ registros totales)"
+        "infoFiltered": "(filtrado de _MAX_ registros totales)"
       }
     };
   }
 
   public myForm!: FormGroup
-  idUser: number 
-  editUser(contenido,user:any){
+  idUser: number
+  editUser(contenido, user: any) {
     console.log(contenido)
     console.log(user)
     this.idUser = user.id
     this.myForm = this.fb.group({
-      username:[user.username],
-      mail:[user.mail],
-      password:[user.password],
-      name:[user.name],
-      type_user:[user.type_user],
-      phone:[user.telefono]
+      username: [user.username],
+      mail: [user.mail],
+      password: [user.password],
+      name: [user.name],
+      type_user: [user.type_user],
+      phone: [user.telefono]
     })
     this.modal.open(
-      contenido,{
-        backdrop:false
-      }
+      contenido, {
+      backdrop: false
+    }
     )
   }
 
 
-  async deleteUser(id: number){
-    if(confirm("Seguro que quieres borrar este usuario?")){
+  async deleteUser(id: number) {
+    if (confirm("Seguro que quieres borrar este usuario?")) {
       const res = await this.adminService.deleteUser(id).toPromise()
       console.log(res)
-      if(res && res.status){
+      if (res && res.status) {
         this.users = this.users.filter(item => item.id !== id)
-      }else{
+      } else {
         alert('Ocurrio un error al eliminar al usuario, intente nuevamente')
       }
 
@@ -87,11 +87,11 @@ export class TableListComponent implements OnInit {
     return true
   }
 
-  async submitFormulary(){
+  async submitFormulary() {
     console.log(this.idUser)
     const res = await this.adminService.editUser(
       this.myForm.value.username,
-      this.myForm.value.pass,
+      this.myForm.value.password,
       this.myForm.value.name,
       this.myForm.value.mail,
       this.myForm.value.type_user,
@@ -100,17 +100,17 @@ export class TableListComponent implements OnInit {
     ).toPromise()
     console.log(res)
     console.log('Usuario editado con exito')
-    if(res && res.status){
+    if (res && res.status) {
       alert("Usuario editado con exito")
       this.modal.dismissAll()
       this.initTable()
-    }else{
+    } else {
       alert("Ocurrio un error intente nuevamente")
     }
   }
 
 }
-interface User{
+interface User {
   id: number,
   name: string,
   username: string
